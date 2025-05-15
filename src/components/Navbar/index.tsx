bf542e704dbd39fae1f43db822d4b09c9574e0f1
0ritam <mujritam085@gmail.com>
2025-05-15 23:12:20 +0530

    Logo

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { cva } from 'class-variance-authority';
+import Link from 'next/link';
+import { Logo } from '../logo';
+
+
+const container = cva(
+  'flex justify-between lg:justify-around items-center px-4 lg:px-8 py-6 z-40',
+);
+
+export function Navbar() {
+
+      return (
+    <div className={container()}>
+      <Link href="/dashboard">
+        
+        <Logo/>
+      </Link>
+      {/* <Menu /> */}
+    </div>
+  );
+
+}
